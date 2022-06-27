23c4237a06a95c68cb332d4b9a8869fdf1b48f04
audryhsu <audry.hsu@gmail.com>
2022-06-26 17:41:36 -0700

    editable CardTitle extracted from Card

--- client/src/components/card/CardTitle.jsx
+++ client/src/components/card/CardTitle.jsx
@@ -1,10 +1,29 @@
 import React from "react"
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { editCard } from "../../features/cards/cards";
 
-const CardTitle = ({title}) => {
+const CardTitle = ({card}) => {
+  const [title, setTitle] = useState(card.title)
+  const dispatch = useDispatch()
+
+  const handleSaveTitle = () => {
+    dispatch(editCard({
+      cardId: card._id,
+      updatedCard: {
+        card: { title }
+      }
+    }))
+  }
+
   return (
     <>
       <i className="card-icon icon .close-modal"></i>
-      <textarea className="list-title" style={{ height: "45px" }} value={title}>
+      <textarea className="list-title" style={{ height: "45px" }}
+      value={title}
+      onChange={ (e) => setTitle(e.target.value) }
+      onBlur={ handleSaveTitle }
+      >
       </textarea>
     </>
   )
